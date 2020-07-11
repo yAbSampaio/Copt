a221a031f29e202e0f8bc528b2929270c02aacf1
Lucas-t-g <49699238+Lucas-t-g@users.noreply.github.com>
2020-07-11 18:16:01 -0300

    Update Swap.cpp

--- Swap.cpp
+++ Swap.cpp
@@ -1,6 +1,3 @@
-// este define é no .h
-#define PI_3_2_RAD 4.71239 //is 3/2pi in rad or 270 degrees
-
 int MarxBotCleaningExperiment::TurnRobot2( float degrees){ //here degrees is the final orientation of the robot
     farsa::ResourcesLocker locker(this);
     farsa::RobotOnPlane* robot =
@@ -12,14 +9,25 @@ int MarxBotCleaningExperiment::TurnRobot2( float degrees){ //here degrees is the
         farsa::Evonet* evonet = getResource<farsa::Evonet>("evonet");
 
     farsa::MarXbot* m_robot = dynamic_cast<farsa::MarXbot*>(robot);
-    // Clock = evonet->getInput(8);
-    
+    Clock = evonet->getInput(8);
+    if( degrees < 0 ){
+        degrees += 360;
+    }
+    if (degrees == 360){
+        degrees = 0;
+    }
+
     std::cout<< "Orientation: " << getAngle() << std::endl;
     
     std::cout<< "Clock: " << Clock << "  ";
     if ( Turning && ( ( abs(getAngle() - degrees) >= 1 ) && ( abs(getAngle() - degrees) <= 359 ) ) ){
         std::cout<< "Girando" << std::endl;
-		m_robot->wheelsController()->setSpeeds(VelPerStepForOneDregrees*last_track, -VelPerStepForOneDregrees*last_track);
+        double vel = (getAngle()-degrees) * VelPerStepForOneDregrees;
+        if (vel > 10){
+            vel = 10;
+        }
+        std::cout<< "vel: " << vel << std::endl;
+		m_robot->wheelsController()->setSpeeds(vel, -vel);
         return 0;
     }else if( Turning ){
         if( ( abs(getAngle() - degrees) < 1 ) && ( abs(getAngle() - degrees) > FAKE_ZERO ) ){
@@ -47,15 +55,4 @@ int MarxBotCleaningExperiment::TurnRobot2( float degrees){ //here degrees is the
         return 0;
 	}
     return 0;
-}
-
-float MarxBotCleaningExperiment::getAngle(){
-    farsa::ResourcesLocker locker(this);
-    farsa::RobotOnPlane* robot =
-        getResource<farsa::RobotOnPlane>("agent[0]:robot");
-    farsa::Arena* arena = getResource<farsa::Arena>("arena");
-    farsa::ResourceVector<farsa::real>* m_additionalInputs;
-    m_additionalInputs = getResource<farsa::ResourceVector<farsa::real> >
-                                            ("agent[0]:additionalInputs");
-    return (robot->orientation(arena->getPlane())+PI_3_2_RAD) * (180 /M_PIf128);
 }
