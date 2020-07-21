75b7b4527530f1a8d0345d69e694304c8ed8f8c9
Thiago Jansen Sampaio Costa <thhiaggo1@hotmail.com>
2020-07-21 18:55:31 -0300

    Update Swap.py

--- Swap.py
+++ Swap.py
@@ -1,5 +1,4 @@
 from matplotlib import pyplot as plt
-import itertools as iter
 import pandas as pd
 import numpy as np
 from io import StringIO
@@ -7,71 +6,46 @@ import csv
 import os
 import subprocess
 
-vec_quartis1 = []
-vec_quartis2 = []
-vec_med = []
-best = []
+dataset = []
+steps = 50000
+Dire = '50_60ks'
+mode = 'Simulação Manual 10-20k de steps'
+stepsDown = 51500
 
-steps = list(range(10000,102000,2000))
-#print(steps)
-Dire = ['10_20ks','30_40ks','50_60ks','70_80ks','90_100ks']
-mode = ['Simulação Manual 10-20k de steps','Simulação Manual 30-40k de steps','Simulação Manual 50-60k de steps','Simulação Manual 70-80k de steps','7imulação Manual 90-100k de steps']
-#stepsUp = [1000,1500,2000]
-#stepsDown = [5000,10000,15000,20000]
-#combi = list(iter.product(stepsUp,stepsDown))
-
-os.chdir("../")
-for mod,folder in enumerate(Dire):
-    os.chdir("{}".format(folder))
-    for steps_i in steps:
-        if os.path.isfile("infos_{}_k_step.csv".format(steps_i)):
-            # opening the data and separating by number of rooms
-            info_Trial = pd.read_csv("infos_{}_k_step.csv".format(steps_i), sep=";")
-
-            values_Fit = info_Trial['total_fitness']
-            total_Trial = values_Fit
-            stati_Data = values_Fit.describe()
-            #print(total_Trial)
-
-            #vec_quartis1.append(stati_Data['25%'])
-            #vec_quartis2.append(stati_Data['75%'])
-            #vec_med.append(stati_Data['50%'])
-            med = stati_Data['50%']
-            quar1 = stati_Data['25%']
-            quar2 = stati_Data['75%']
-
-
-            #Boxplot startup and analysis
-            s_q1 = []
-            s_mediana = []
-            s_q2 = []
-            legenda = "Steps".format(steps_i)
-
-
-
-
-            s_q1.append("{0:.2f}".format(quar1))
-            s_mediana.append("{0:.2f}".format(med))
-            s_q2.append("{0:.2f}".format(quar2))
-        
-            font_1 = {'family': 'serif', 'color': '#5C1BCC', 'size':'14'}
-
-            plt.figure(figsize=(25, 15))
-            plt.boxplot(total_Trial)
-            plt.xticks([1], legenda)
-            plt.title('Boxplot Fitness - Modo {} - {}k Steps'.format(mode[mod],steps_i))
-            plt.ylabel('% de Limpeza')
-            plt.xlabel('Tempo de Subida e Descida')
-
+while steps < 51000:
+    print(steps)
+    os.chdir("../{}".format(Dire))
+    with open("trage_s{}_k_step.csv".format(steps)) as pointer:
+        plain_text = pointer.read()
+        payload = plain_text.split("=")
+        print(len(payload))
+        trial_info = pd.read_csv("infos_{}_k_step.csv".format(steps), sep=";")
+        trials = trial_info['total_fitness']
+        trial_infos = trials[trials>0.90]
+        for i, trial in enumerate(payload):
+            if len(trial) > 0:
+                dataset.append([pd.read_csv(StringIO(trial), sep=";"), trial_infos])
+        print(len(dataset))
+        chunks = 5
+        size_ms = 1
+        colors = ['red', 'blue', 'gray', 'magenta', 'green']
+        for trial, data in enumerate(dataset):
+            print(trial)
+            times = data[0]['x'].size // chunks
+            fig, ax = plt.subplots(figsize=(10, 10))
+            labels, lines = [], []
+            total_fitness = data[1]*100
+            aux = total_fitness.tolist()
             
-            plt.text(1, quar1, s_q1[0], fontdict=font_1)
-            plt.text(1, med, s_mediana[0], fontdict=font_1)
-            plt.text(1, quar2, s_q2[0], fontdict=font_1)
-            plt.savefig('../plot/{}K_boxplot.png'.format(steps_i))
-        #s_q1.clear()
-        #s_q2.clear()
-        #s_mediana.clear()
-        #vec_med.clear()
-        #vec_quartis1.clear()
-        #vec_quartis2.clear()
-    os.chdir("../")
+            ax.set_title('Posição do Robô (Trial {}) - {:.2f}% limpo.'.format(trial+1, aux[trial]))
+            for it in range(chunks):
+                batch = data[0][['x','y']].loc[it*times:(it+1)*times]
+                ax.scatter(batch['x'], batch['y'], c=colors[it], s=0.1, alpha=1)
+                lines.append(plt.Line2D([0], [0], color=colors[it], lw=4))
+                labels.append("step $\exists$ [{},{}]".format(it*times, (it+1)*times))
+            ax.legend(lines, labels,  bbox_to_anchor = (0,-0.1,1,1))
+            plt.savefig('../plot/trage/trage_Stps_{}_.png'.format(steps), dpi=100)
+    dataset.clear()
+    steps += 2000
+    #stepsDown = int((steps/5)-4500)
+    os.chdir("../cod_plots")
